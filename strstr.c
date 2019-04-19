2f389bbec7ab359499d170e834678161bd1dba07
VT Freshers <48670544+VTfreshers@users.noreply.github.com>
2019-04-19 12:50:04 +0530

    strstr.c

---
+++ strstr.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+char *rev(char *ptr)
+{
+	static char rev[20];
+	static int i;
+	int len=strlen(ptr),cnt;
+	if(i>0) return rev;
+	for(cnt=len,i=0;cnt--;i++)
+		rev[len-i-1]=ptr[i];
+	rev[i]=0;
+	return rev;
+}
+
+int main(int argc,char *argv[])
+{
+	if(argc<3)
+		printf("pattern is <./a.out str1 str2>");
+	else
+	{
+		char *temp=rev(argv[2]),*p;
+		while(p=strstr(argv[1],buf))
+		{
+			printf("found at %ld\n",p-argv[1]);
+			memmove(p,temp,strlen(temp));
+			p++;
+		}
+	}
+	printf("%s\n",argv[1]);
+	return 0;
+}
